eecab25ca0d9827cd9e8a0b67aeabdd5bae5b88e
Cyril <dubernard.cyril@gmail.com>
2024-11-01 17:45:10 +0100

    Ajout de la page d'antennes

---
+++ antennes.js
@@ -0,0 +1,132 @@
+export const listeAntennes = [
+    {
+        nom: 'France Depression Normandie',
+        adresse: '10 Rue Saint-Julien',
+        ville: 'Rouen',
+        CP: '76100',
+        page:"                        <p class=\"fld_adresse\">\n" +
+            "                Maison des Associations 10-12 rue Saint Julien                       \n" +
+            "        <br>76000&nbsp;                          \n" +
+            "        Rouen        </p>\n" +
+            "                                <p class=\"fld_tel1\">\n" +
+            "                               \n" +
+            "        07 71 77 72 35&nbsp;\n" +
+            "        </p>\n" +
+            "\n" +
+            "                <p class=\"fld_mel\">\n" +
+            "                <joomla-hidden-mail is-link=\"1\" is-email=\"1\" first=\"ZnJhbmNlZGVwcmVzc2lvbi5ub3JtYW5kaWU=\" last=\"eWFob28uZnI=\" text=\"ZnJhbmNlZGVwcmVzc2lvbi5ub3JtYW5kaWVAeWFob28uZnI=\" base=\"\"><a href=\"mailto:francedepression.normandie@yahoo.fr\" base=\"\">francedepression.normandie@yahoo.fr</a></joomla-hidden-mail></p>\n" +
+            "        <p><span style=\"font-size: 14pt;\"><strong>Calendrier à partir de SEPTEMBRE 2023</strong></span></p>\n" +
+            "<p><strong><span style=\"font-size: 12pt;\">Écoute téléphonique nationale au 07 84 96 88 28</span><i>&nbsp;</i></strong><strong><em>du lundi au samedi&nbsp;</em></strong><strong><em>de 10h à 20h, et</em></strong><i><b><b>&nbsp;le dimanche de 14h à 20h</b><i></i>.</b></i></p>\n" +
+            "<p><span style=\"font-size: 12pt;\"><b>&nbsp;</b></span></p>\n" +
+            "<p><b>Prochaines rencontres :</b></p>\n" +
+            "<p>Les rencontres ont lieu à <b><span style=\"text-decoration: underline;\">Rouen</span></b>: Maison des Associations - 10-12 rue Saint Julien 76100 Rouen St Sever</p>\n" +
+            "<p><span style=\"font-size: 12pt;\"><b>Groupe de médiation ou de parole</b></span></p>\n" +
+            "<p><strong>Lundi 25 septembre 2023 de 18h00 à 20h00</strong></p>\n" +
+            "<p>Puis tous les 1er et 4ème lundi de chaque mois de 18h00 à 20h00.</p>\n" +
+            "<p>&nbsp;</p>\n" +
+            "<p><strong><span style=\"font-size: 12pt;\">Groupe de parole</span></strong></p>\n" +
+            "<p><b>Tous les 2ème jeudi de chaque mois de 18h30 à 20h00.</b></p>\n"
+    },
+    {
+        nom: 'France Dépression Grand Paris Île-de-France',
+        adresse: '9 Rue Rés Clos du Chateau',
+        ville: 'Ballainvilliers',
+        CP: '91160',
+        page: "<p class=\"fld_adresse\"> Adresse Administrative : 9, Clos du château,  91160, Ballainvilliers<br>\n" +
+            "Téléphone Administratif : 06 02 52 66 02 <br>\n" +
+            "</p>\n" +
+            "                   \n" +
+            "<p class=\"fld_mel\">\n" +
+            "\t<a href=\"mailto:paris@francedepression.fr\" base=\"\">paris@francedepression.fr</a>\n" +
+            "</p>\n" +
+            "\n" +
+            "\n" +
+            "<p><a href=\"http://www.facebook.com/francedepression\" target=\"_blank\"><i class=\"fab fa-facebook-square\"></i> www.facebook.com/francedepression</a></p>\n" +
+            "<br>\n" +
+            "<p><span style=\"font-size: 14pt; background-color: #ffff00;\">Ecoute patients : 07 84 96 88 28</span></p>\n" +
+            "<br>\n" +
+            "<p><span style=\"color: #808080; font-family: arial, helvetica, sans-serif; font-size: 18pt;\">Pour Devenir A<span style=\"background-color: #ffffff;\">dhér</span>ent de FD Paris Ile-de-France,&nbsp;<span style=\"color: #808080;\">cliquer sur le lien ci-dessou</span>s</span></p>\n" +
+            "<p style=\"padding-left: 30px;\"><span style=\"background-color: #000080; color: #ffffff; font-size: 12pt;\"><a href=\"https://www.helloasso.com/associations/france-depression-grand-paris-ile-de-france/adhesions/adhesionglissante\" style=\"background-color: #000080; color: #ffffff;\">https://www.helloasso.com/associations/france-depression-grand-paris-ile-de-france/adhesions/adhesionglissante</a></span></p> \n"
+    },
+    {
+        nom: 'France Dépression Lorraine',
+        adresse: '10 Rue de Norvège',
+        ville: 'Vandœuvre-lès-Nancy',
+        CP: '54500',
+        page:"<p class=\"fld_adresse\">\n" +
+            "    Association France Dépression Lorraine<br>\n" +
+            "    ADRESSE POSTALE<br>\n" +
+            "    10 rue de Norvège<br>\n" +
+            "    54500 Vandœuvre-lès-Nancy\n" +
+            "</p>\n" +
+            "\n" +
+            "<p class=\"fld_tel1\">06 03 89 34 47</p>\n" +
+            "\n" +
+            "<p class=\"fld_mel\">\n" +
+            "    <a href=\"mailto:contact@francedepression.fr\">contact@francedepression.fr</a>\n" +
+            "</p>\n" +
+            "\n" +
+            "<p>\n" +
+            "    <a href=\"https://www.facebook.com/France-D%C3%A9pression-Lorraine-712825752081779/\" target=\"_blank\" rel=\"noopener\">\n" +
+            "        <i class=\"fab fa-facebook-square\" style=\"font-size: 1.5rem;\"></i> Retrouvez-nous sur Facebook\n" +
+            "    </a>\n" +
+            "</p>\n" +
+            "\n" +
+            "<p>L'Association France Dépression Lorraine a vu le jour le 17 novembre 2002. Créée par Claudie Tondon-Bernard, elle-même malade dépressive, l’association s'est développée grâce à une équipe active de bénévoles : malades dépressifs et bipolaires, familles, proches, et médecins.</p>\n" +
+            "\n" +
+            "<details><summary>Lire la suite...</summary>\n" +
+            "<p>\n" +
+            "    <p>C'est grâce à ce réseau et à ce travail collectif que France Dépression Lorraine vit et se développe. Notre Parrain, David Servan Schreiber, a soutenu l'association dès ses débuts. Sa cousine Florence Servan Schreiber a repris le relais avec la même conviction. Merci à toutes ces personnes pour leur aide précieuse.</p>\n" +
+            "    <p>France Dépression Lorraine ne prétend pas remplacer l'aide médicale, mais assure une complémentarité d'écoute, de soutien et d'information pour éviter l'exclusion et l'isolement des malades, et encourager la compréhension de leurs proches. Recréer un lien social est l'un des objectifs principaux de notre association.</p>\n" +
+            "    <p>Nos autres buts sont :</p>\n" +
+            "    <ul>\n" +
+            "        <li>Soutenir et informer les malades sur la dépression et la maladie bipolaire.</li>\n" +
+            "        <li>Susciter la compréhension du grand public et combattre les \"a priori\" sur la maladie mentale.</li>\n" +
+            "    </ul>\n" +
+            "    <p>N'hésitez pas à nous contacter, à nous rejoindre, et à partager cette belle histoire. Le partage est important, et plus nous serons nombreux, plus notre voix pourra être entendue.</p>\n" +
+            "</p></details>\n"
+    },
+    {
+        nom: 'France Dépression Savoie',
+        adresse: '58 Rue Dr Blain',
+        ville: 'La Motte-Servolex',
+        CP: '73290',
+        page:"<p class=\"fld_adresse\">\n" +
+            "    Adresse administrative : <br>\n" +
+            "    FRANCE DÉPRESSION SAVOIE<br>\n" +
+            "    58, rue Docteur Blain<br>\n" +
+            "    73290 La Motte Servolex\n" +
+            "</p>\n" +
+            "\n" +
+            "<p class=\"fld_personne-a-contacter\">Contacter : Thierry</p>\n" +
+            "<p class=\"fld_tel1\">07 66 29 67 20</p>\n" +
+            "<p class=\"fld_mel\">\n" +
+            "    <a href=\"mailto:savoie@francedepression.fr\">savoie@francedepression.fr</a>\n" +
+            "</p>\n" +
+            "\n" +
+            "<div>\n" +
+            "    <a href=\"https://www.facebook.com/francedepressionsavoie/\">Suivez-nous sur Facebook</a>\n" +
+            "</div>\n" +
+            "\n" +
+            "<p>Ligne d'écoute téléphonique : 07 84 96 88 28<br>\n" +
+            "    du lundi au samedi de 10 h à 20 h, dimanche de 14 h à 20 h</p>\n" +
+            "\n" +
+            "<p>France Dépression soutient les personnes souffrant de troubles de l'humeur, les informant et les aidant à sortir de l'isolement.</p>\n" +
+            "\n" +
+            "<p>Notre association organise des activités de sensibilisation à la santé mentale et de déstigmatisation.</p>\n"
+
+    },
+    {
+        nom: 'France Dépression Var',
+        adresse: 'Bd des Fleurs',
+        ville: 'Draguignan',
+        CP: '83300',
+        page: "<p class=\"fld_mel\">\n" +
+            "    <a href=\"mailto:var@francedepression.fr\">var@francedepression.fr</a>\n" +
+            "</p>\n" +
+            "\n" +
+            "<p>FRANCE DEPRESSION VAR</p>\n" +
+            "<p>Le lundi de 11h à 14h.<br>Club Léo Lagrange<br>Salle B<br>DRAGUIGNAN</p>\n" +
+            "<p>Adhésion : 20 € par an<br>Dons : Défiscalisation</p> \n"
+    }
+]
---
+++ pageAntennes.js
@@ -0,0 +1,34 @@
+import {listeAntennes} from './antennes.js';
+
+const elt = document.querySelector('.txtRecherche');
+elt.addEventListener('input',
+    (e)=>document.querySelector('.partieDetails').textContent = afficherResultatRecherche(elt.value));
+
+elt.dispatchEvent(new Event('input'));
+function correspond(antenne, saisie) {
+    return antenne.ville.toLowerCase().includes(saisie) ||
+        antenne.CP.includes(saisie) ||
+        antenne.nom.toLowerCase().includes(saisie);
+}
+
+function recherche(saisie){
+    return listeAntennes.filter((antenne) => correspond(antenne, saisie.toLowerCase()))
+}
+
+function afficherResultatRecherche(saisie){
+    const templateAntenne = document.querySelector("#antenne");
+    const listeAnt = document.querySelector("#listeAntennes");
+
+    listeAnt.innerHTML = '';
+
+    recherche(saisie).forEach(elt => {
+        const copie = templateAntenne.content.cloneNode(true);
+
+        copie.querySelector("#nomAntenne").textContent = elt.nom+', ';
+        copie.querySelector("#villeCPAntenne").textContent = elt.CP + ' ' + elt.ville;
+        copie.querySelector('p').innerHTML = elt.page;
+
+        listeAnt.appendChild(copie);
+    });
+
+}
