4c3ec0363228e4362e18e337644eb2fa6fffaa0c
fuckle17 <komatigmz@gmail.com>
2024-11-10 00:56:53 +0100

    ajout du menu burger

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ buttonClose.addEventListener('click', () => {
     document.querySelector('#barre_info').style.display = 'none';
     document.querySelector('section').style.paddingTop = '120px';
 })
+
+const buttonMenuMobile = document.querySelector('#buttonMenu')
+const MenuAssociation = document.querySelector('#menu-association')
+
+buttonMenuMobile.addEventListener('click', () => {
+    const menu = document.querySelector('li > a')
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+})
+
+MenuAssociation.addEventListener('mouseover', () => {
+    document.querySelector('#menu-association').style.display = 'block'
+})
+
+document.addEventListener('DOMContentLoaded', responsiveBanner)
+window.addEventListener('resize', responsiveBanner)
+
+function responsiveBanner() {
+    const navbar = document.querySelector('nav')
+    const button_menu = document.querySelector('#buttonMenu')
+    if (window.innerWidth < 900) {
+        navbar.style.display = 'none'
+        button_menu.style.display = 'block'
+    } else {
+        navbar.style.display = 'flex'
+        button_menu.style.display = 'none'
+        document.querySelector('#menumobile').style.display = 'none';
+    }
+}
