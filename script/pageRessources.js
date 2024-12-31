023cd735e9f5677069196f9f5cfe4236c8ae7fda
Cyril <dubernard.cyril@gmail.com>
2024-12-31 16:18:15 +0100

    Ajout page ressources, il reste à l'alimenter en données (dans ressources.js)

---
+++ script/pageRessources.js
@@ -0,0 +1,12 @@
+import {listeRessources} from './ressources.js';
+
+const templateRessource = document.querySelector("#ressource-template");
+const listeRes = document.querySelector("#listeRessources");
+listeRessources.forEach(elt => {
+    const copie = templateRessource.content.cloneNode(true);
+    copie.querySelector(".ressource-title").textContent = elt.title;
+    copie.querySelector(".ressource-description").textContent = elt.desc;
+    copie.querySelector('.ressource-type').textContent = elt.type;
+
+    listeRes.appendChild(copie);
+});
---
+++ script/ressources.js
@@ -0,0 +1,26 @@
+export const listeRessources = [
+    {
+        title : 'Ressources',
+        desc : 'reste a alimenter avec quelques ressources pour remplir un peu (données a mettre dans le fichier ressources.js)',
+        type : 'Article'
+    },
+    {
+        title : 'Titre2',
+        desc : 'Desc2',
+        type : 'Ty'
+    },
+    {
+        title : 'Titre3',
+        desc : 'Desc3',
+        type : 'Typ'
+    },
+    {
+        title : 'Titre4',
+        desc : 'Desc4',
+        type : 'Type'
+    },
+    {
+        title : 'Titre5',
+        desc : 'Desc5',
+        type : 'Type5'
+    },]
